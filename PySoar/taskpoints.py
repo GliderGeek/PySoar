7c206ab16f41bc1440d32cdcce75e9bf8e4259d8
GliderGeek <GliderGeek@users.noreply.github.com>
2016-06-28 20:28:56 +0200

    Adding license GNU GPL, close #53

--- PySoar/taskpoints.py
+++ PySoar/taskpoints.py
@@ -128,3 +128,21 @@ class Taskpoint(object):  # startpoint, turnpoints and finish
                     return False
                 else:  # distance2 <= self.r_max
                     return angle_wrt_orientation2 < self.angle_max
+
+#############################  LICENSE  #####################################
+#																			#
+#   PySoar - Automating gliding competition analysis						#
+#   Copyright (C) 2016  Matthijs Beekman									#
+# 																			#
+#   This program is free software: you can redistribute it and/or modify	#
+#   it under the terms of the GNU General Public License as published by	#
+#   the Free Software Foundation, either version 3 of the License, or		#
+#   (at your option) any later version.										#
+# 																			#
+#   This program is distributed in the hope that it will be useful,			#
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of			#
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the			#
+#   GNU General Public License for more details.							#
+# 																			#
+#   You should have received a copy of the GNU General Public License		#
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>	#
--- readme.md
+++ readme.md
@@ -25,5 +25,22 @@ For development, the following steps need to be taken:
 pip install -r requirements.txt
 ```
 
+## License
+
+	PySoar - Automating gliding competition analysis	Copyright (C) 2016  Matthijs Beekman
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>
 
 
+You can find the full license text in the [LICENSE](LICENSE) file.
